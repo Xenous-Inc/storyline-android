e3b3739b858774eb2b5c9ee19bf4fb9de0a035a6
d3faultik <sheremetyev48@mail.ru>
2020-05-02 18:11:07 +0300

    Quotes Activity Recycler View Commit -Добавление Recycler View для цитат на страничке цитат

--- app/src/main/java/com/xenous/storyline/activities/MainActivity.kt
+++ app/src/main/java/com/xenous/storyline/activities/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "MainActivity"
         const val QUOTE_TAG = "StoryFragment : Quote"
         
-        const val AVAILABLE_QUOTE_LENGTH = 5
+        const val AVAILABLE_QUOTE_LENGTH = 35
     }
     
     private lateinit var fragmentFrameLayout: FrameLayout
@@ -82,6 +82,7 @@ class MainActivity : AppCompatActivity() {
                 
                 storyLayout.setCoverImageResource(R.drawable.demo_background)
                 storyLayout.cover.setOnClickListener {
+                    startActivity(Intent(this@MainActivity, QuotesActivity::class.java))
                     storyLayout.collapseStoryCover()
                 }
                 storyLayout.setContentFragment(storyFragment!!, supportFragmentManager)

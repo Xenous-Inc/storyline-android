d2ed681adcecf29e6221230d93d95b2d58c78f27
d3faultik <sheremetyev48@mail.ru>
2020-05-02 22:57:51 +0300

    Activity Registration Details Commit -добавлена возможность выбора интересов и установки своего имени после регистрации нового аккауньа

--- app/src/main/java/com/xenous/storyline/activities/RegistrationDetailsActivity.kt
+++ app/src/main/java/com/xenous/storyline/activities/RegistrationDetailsActivity.kt
@@ -1,25 +1,37 @@
 package com.xenous.storyline.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Message
+import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.pranavpandey.android.dynamic.toasts.DynamicToast
 import com.xenous.storyline.R
+import com.xenous.storyline.data.User
 import com.xenous.storyline.utils.ERROR_CODE
 import com.xenous.storyline.utils.SUCCESS_CODE
 
 class RegistrationDetailsActivity : AppCompatActivity() {
+    
+    private companion object {
+        const val TAG = "RegistrationDetails"
+    }
 
     private lateinit var authentication : FirebaseAuth
 
     private lateinit var nameEditText : EditText
     private lateinit var interestsLinearLayout : LinearLayout
     private lateinit var nextTextView : TextView
+    private lateinit var russianClassicalLiteratureCheckBox : CheckBox
+    private lateinit var russianModernLiteratureCheckbox : CheckBox
+    private lateinit var foreignClassicLiteratureCheckbox : CheckBox
+    private lateinit var foreignModernLiteratureCheckbox : CheckBox
 
     private lateinit var sendUsersDetailsToDatabaseHandler : Handler
 
@@ -31,73 +43,110 @@ class RegistrationDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_details)
-
-        authentication = FirebaseAuth.getInstance()
-        if(authentication.currentUser == null) {
-            onBackPressed()
-
-            Toast.makeText(this, "Произошла неизвестная ошибка", Toast.LENGTH_SHORT).show()
-        }
-
+        
         nameEditText = findViewById(R.id.nameEditText)
         interestsLinearLayout = findViewById(R.id.interestsLinearLayout)
         nextTextView = findViewById(R.id.nextTextView)
-
-        val interestsTexts = listOf(
-            "Русская классическая литература",
-            "Русская современная литература",
-            "Зарубежная классическая литература",
-            "Зарубежная современная литература"
+        russianClassicalLiteratureCheckBox = findViewById(R.id.russianClassicLiteratureCheckbox)
+        russianModernLiteratureCheckbox = findViewById(R.id.russianModernLiteratureCheckbox)
+        foreignClassicLiteratureCheckbox = findViewById(R.id.foreignClassicLiteratureCheckbox)
+        foreignModernLiteratureCheckbox = findViewById(R.id.foreignModernLiteratureCheckbox)
+        
+        setUpCheckBoxListener()
+        
+        checkUser()
+        
+        nextTextView.setOnClickListener {view ->
+            if(isNameEmpty()) {
+                DynamicToast.makeWarning(this, getString(R.string.not_fully_necessary))
+                
+                return@setOnClickListener
+            }
+            
+            val userInterests = arrayListOf<Long>()
+            
+            for(userAnswersCounter in userAnswers.indices) {
+                if(userAnswers[userAnswersCounter] > -1) {
+                    userInterests.add(userAnswers[userAnswersCounter].toLong())
+                }
+            }
+            
+            if(userInterests.size == 0) {
+                Log.d(TAG, "User has not chosen anything")
+                DynamicToast.makeWarning(this, getString(R.string.not_fully_necessary), Toast.LENGTH_SHORT).show()
+                
+                return@setOnClickListener
+            }
+            
+            val user = User(
+                nameEditText.text.toString(),
+                userInterests,
+                mapOf(
+                    "last_date" to 0,
+                    "level" to 0,
+                    "streak" to 0
+                )
+            )
+    
+            initSendUsersDetailsToDatabaseHandler()
+            
+            SendUsersDetailsToDataBaseThread(user).start()
+            
+            view.isClickable = false
+        }
+    }
+    
+    @SuppressLint("HandlerLeak")
+    private fun initSendUsersDetailsToDatabaseHandler() {
+        sendUsersDetailsToDatabaseHandler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                
+                nextTextView.isClickable = true
+                
+                when(msg.what) {
+                    SUCCESS_CODE -> startActivity(Intent(this@RegistrationDetailsActivity, MainActivity::class.java))
+                    ERROR_CODE -> DynamicToast.makeError(this@RegistrationDetailsActivity, getString(R.string.unknown_error), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    
+    private fun checkUser() {
+        authentication = FirebaseAuth.getInstance()
+        if(authentication.currentUser == null) {
+            onBackPressed()
+        
+            Toast.makeText(this, "Произошла неизвестная ошибка", Toast.LENGTH_SHORT).show()
+        }
+    }
+    
+    private fun setUpCheckBoxListener() {
+        val checkBoxes = listOf(
+            russianClassicalLiteratureCheckBox,
+            russianModernLiteratureCheckbox,
+            foreignClassicLiteratureCheckbox,
+            foreignModernLiteratureCheckbox
         )
-
-        for(checkBoxCounter in 0..3) {
-            val checkBox = CheckBox(this)
-            checkBox.text = interestsTexts[checkBoxCounter]
-            checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
+        
+        for(checkBoxCounter in checkBoxes.indices) {
+            checkBoxes[checkBoxCounter].setOnCheckedChangeListener { _, isChecked ->
                 if(isChecked) {
+                    Log.d(TAG, "The interest number $checkBoxCounter has been chosen")
+                    
                     userAnswers[checkBoxCounter] = checkBoxCounter
                 }
                 else {
+                    Log.d(TAG, "The interest number $checkBoxCounter has been unchosen")
+                    
                     userAnswers[checkBoxCounter] = -1
                 }
             }
-
-            interestsLinearLayout.addView(checkBox)
         }
-
-        sendUsersDetailsToDatabaseHandler = object : Handler() {
-    
-        }
-
-        nextTextView.setOnClickListener {
-            if(isAllNecessaryFieldsEmpty()) {
-                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                val userInterests = arrayListOf<Long>()
-
-                for(userInterestsCounter in 0 until userAnswers.size) {
-                    if(userAnswers[userInterestsCounter] > -1) {
-                        userInterests.add(userAnswers[userInterestsCounter].toLong())
-                    }
-                }
-            }
-        }
-
     }
 
-    private fun isAllNecessaryFieldsEmpty() : Boolean {
-        var isUsersAnswersListEmpty = false
-
-        for(userAnswersCounter in 0 until userAnswers.size) {
-            if(userAnswers[userAnswersCounter] > -1) {
-                isUsersAnswersListEmpty = true
-
-                break
-            }
-        }
-
-        if(nameEditText.text.isBlank() || !isUsersAnswersListEmpty) {
+    private fun isNameEmpty() : Boolean {
+        if(nameEditText.text.isEmpty()) {
             return true
         }
 
@@ -105,14 +154,14 @@ class RegistrationDetailsActivity : AppCompatActivity() {
     }
 
     inner class SendUsersDetailsToDataBaseThread(
-        private val user : FirebaseUser
+        private val user : User
     ) : Thread() {
         override fun run() {
             super.run()
 
             Firebase.firestore
                 .collection("users")
-                .document(authentication.currentUser.toString())
+                .document(authentication.currentUser!!.uid.toString())
                 .set(user)
                 .addOnSuccessListener {
                     sendUsersDetailsToDatabaseHandler.sendEmptyMessage(SUCCESS_CODE)
--- app/src/main/java/com/xenous/storyline/data/User.kt
+++ app/src/main/java/com/xenous/storyline/data/User.kt
@@ -3,12 +3,11 @@ package com.xenous.storyline.data
 data class User(
     val nickname : String? = null ,
     val interests : List<Long>? = null,
-    val quotes_paths : List<String>? = null,
     val stats : Map<String, Any>? = null
 ) {
 
     override fun toString(): String {
-        return "User's name is $nickname. \n User's interests are $interests. Paths to quotes are $quotes_paths. User's last date is $stats"
+        return "User's name is $nickname. \n User's interests are $interests. User's last date is $stats"
     }
 
 }
